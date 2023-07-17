9cc24f4b78634b0af38c519212ffb7e29e11a89d
arturoitnora <122922316+Arturo-itnora@users.noreply.github.com>
2023-07-17 15:12:17 -0600

    connected to cloud mysql database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 // config/db.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('elearning', 'admin', '1q2w3e4r', {
-  host: 'localhost',
+const sequelize = new Sequelize('railway', 'root', 'OOEYU9X4HlpI7dtKaniu', {
+  host: 'containers-us-west-111.railway.app',
   dialect: 'mysql',
+  port: '7776',
 });
 
 module.exports = { sequelize };
