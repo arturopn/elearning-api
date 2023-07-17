e31ed31f5b081bce7904754135f4134d6854c98c
arturoitnora <122922316+Arturo-itnora@users.noreply.github.com>
2023-07-17 15:38:04 -0600

    change connection again

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ const tablesExist = async () => {
 
 // Create a user, themes, and content on server startup if tables do not exist
 sequelize.sync().then(async () => {
-  const tablesAlreadyExist = await tablesExist();
-  if (!tablesAlreadyExist) {
+ // const tablesAlreadyExist = await tablesExist();
+ // if (!tablesAlreadyExist) {
     try {
       // Create a user
       const user = await User.create({
@@ -97,7 +97,7 @@ sequelize.sync().then(async () => {
     } catch (error) {
       console.error('Error during initial database setup:', error);
     }
-  }
+ // }
 
   // Start the server
   app.listen(port, () => {
